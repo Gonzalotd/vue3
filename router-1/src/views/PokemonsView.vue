5ca1ad7e6e74593b28f26b8840560af59a74151b
Gonzalotd <54744142+Gonzalotd@users.noreply.github.com>
2025-10-26 18:53:03 +0100

    composables
    update file with composables

--- router-1/src/views/PokemonsView.vue
+++ router-1/src/views/PokemonsView.vue
@@ -1,30 +1,44 @@
 <script setup>
 
-    import axios from 'axios';
+    // import axios from 'axios';
     import { ref } from 'vue';
     import { RouterLink } from 'vue-router';
-    
-    const pokemons = ref([])
-    const getData = async () => {
-        try {
-            const { data } = await axios.get('http://pokeapi.co/api/v2/pokemon')
-            pokemons.value = data.results;
+    import { useGetData } from '@/composables/getData';
 
-        } catch (error) {
-            console.log(error)
-        }
-    }
+    // const pokemons = ref([]);
+    const { data, loading, getData, errorData } = useGetData()
 
-    getData();
+    // const getData = async () => {
+    //     try {
+    //         const { data } = await axios.get('http://pokeapi.co/api/v2/pokemon')
+    //         pokemons.value = data.results;
+
+    //     } catch (error) {
+    //         console.log(error)
+    //     }
+    // }
+
+    getData('http://pokeapi.co/api/v2/pokemon');
 </script>
 
 <template>
     <h1>Pokemons</h1>
-    <ul >
-        <li v-for="poke in pokemons">
-            <router-link :to="`/pokemons/${poke.name}`">
-                {{ poke.name }}
-            </router-link>
-        </li>
-    </ul>
+    <p v-if="loading">Cargando informacion</p>
+    <div class="alert alert-danger mt-2" v-if="errorData">{{ errorData }}</div>
+    <div v-if="data">
+        <ul class="list-group">
+            <!-- <li v-for="poke in pokemons"> -->
+            <li class="list-group-item" v-for="poke in data.results">
+                <router-link :to="`/pokemons/${poke.name}`">
+                    {{ poke.name }}
+                </router-link>
+            </li>
+        </ul>
+        <div class="mt-2">
+            <button :disabled="!data.previous" 
+                class="btn btn-warning me-3" @click="getData(data.previous)">Previus</button>
+            <button :disabled="!data.next"
+                class="btn btn-warning" @click="getData(data.next)">Next</button>
+        </div>
+    </div>
 </template>
